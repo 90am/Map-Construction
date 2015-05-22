ac0c6a904bcdbdada3b29500d0d5ec2a9fe41362
Andreas Mathisen <mathisen90@msn.com>
2015-05-22 14:18:49 +0200

    Updated addSegment method

--- Map Construction Java/src/Grid3.java
+++ Map Construction Java/src/Grid3.java
@@ -73,39 +73,17 @@ public class Grid3 {
         return util.formatGridPositions(computeCurves(), xPixelWidth, yPixelWidth, xMin, yMin);
     }
 
-    public HashMap<Integer, ArrayList<GridPosition>> getComponents(){
-        HashMap<Integer, ArrayList<GridPosition>> components = new HashMap<Integer, ArrayList<GridPosition>>();
-        HashSet<GridPosition> addedToComponent = new HashSet<GridPosition>();
-        int componentId = 1;
-        for(GridPosition g : gridValues.keySet()){
-            if(!addedToComponent.contains(g)){
-                HashSet<GridPosition> visited = new HashSet<GridPosition>();
-                ArrayList<GridPosition> component = new ArrayList<GridPosition>();
-                visited.add(g);
-                LinkedList<GridPosition> toVisit = new LinkedList<GridPosition>();
-                toVisit.add(g);
-                component.add(g);
-                addedToComponent.add(g);
-                int angle = maxAng(g);
-                if(gridValues.get(g)[angle] > 2){
-                    while(toVisit.size() > 0){
-                        GridPosition current = toVisit.poll();
-                        for(GridPosition neighbour : getNeighborsWithProbability(current)){
-                            if(!visited.contains(neighbour)){
-                                visited.add(neighbour);
-                                if(angle == maxAng(neighbour) && gridValues.get(neighbour)[angle] > 2){
-                                    toVisit.add(neighbour);
-                                    component.add(neighbour);
-                                    addedToComponent.add(neighbour);
-                                }
-                            }
-                        }
-                    }
-                    components.put(componentId++, component);
-                }
+    private void addNeighborProbability(Point p, int angleIndex){
+        GridPosition g = getGridPosition(p);
+        ArrayList<GridPosition> neighbors = getNeighbors(g);
+        double[] angleArray = new double[angles];
+        for(GridPosition n : neighbors){
+            if(gridValues.containsKey(n)){
+                angleArray =  gridValues.get(n);
             }
+            angleArray[angleIndex]++;
+            gridValues.put(n, angleArray);
         }
-        return components;
     }
 
     public void addSegment(Point a, Point b){
@@ -117,16 +95,14 @@ public class Grid3 {
         }
         double ang = util.getAngle(p1, p2);
         int angIdx = (int)Math.floor((angles * ((ang + Math.PI / (angles * 2)) / (Math.PI)))) % angles;
+        addNeighborProbability(p1, angIdx);
+        addNeighborProbability(p2, angIdx);
         int x = (int)((p1.getX()-xMin)/xPixelWidth);
         int y = (int)((p1.getY()-yMin)/yPixelWidth);
         int xStop = (int)((p2.getX()-xMin)/xPixelWidth);
         int yStop = (int)((p2.getY()-yMin)/yPixelWidth);
         int xSteps = Math.abs(xStop-x);
         int ySteps = Math.abs(yStop-y);
-        //System.out.println("Point "+a.getNewX()+","+a.getY());
-        //System.out.println("Point "+b.getNewX()+","+b.getY());
-        //System.out.println("X steps " + xSteps);
-        //System.out.println("Y steps " + ySteps);
         if(ySteps >= xSteps){
             int step = 0;
             double xChange = ySteps == 0 ? 0 : (p2.getX() - p1.getX()) / ySteps;
@@ -155,7 +131,6 @@ public class Grid3 {
                 y = (int) ((yVal-yMin)/yPixelWidth);
                 GridPosition g = new GridPosition(x, y);
                 double[] angl;
-                //System.out.println("x,y: " + x+","+y);
                 if(!gridValues.containsKey(g)){
                     angl = new double[angles];
                     gridValues.put(g,angl);
@@ -172,14 +147,14 @@ public class Grid3 {
     }
 
 
-    public HashMap<Integer, ArrayList<GridPosition>> getComponents2(){
+    public HashMap<Integer, ArrayList<GridPosition>> getComponents(){
         HashMap<GridPosition, double[]> dataSet = gridValues;
         int componentId = 0;
         HashMap<Integer, ArrayList<GridPosition>> components = new HashMap<Integer, ArrayList<GridPosition>>();
         for(GridPosition g : dataSet.keySet()){
             double[] angles = dataSet.get(g);
             for(int i=0; i<8; i++) {
-                if(angles[i] > 2){
+                if(angles[i] > 5){
                     HashSet<GridPosition> visited = new HashSet<GridPosition>();
                     ArrayList<GridPosition> component = new ArrayList<GridPosition>();
                     LinkedList<GridPosition> toVisit = new LinkedList<GridPosition>();
@@ -192,7 +167,7 @@ public class Grid3 {
                         for (GridPosition n : getNeighborsWithProbability(current)) {
                             if (!visited.contains(n)) {
                                 visited.add(n);
-                                if (dataSet.get(n)[i] > 2) {
+                                if (dataSet.get(n)[i] > 5) {
                                     toVisit.add(n);
                                     component.add(n);
                                     dataSet.get(n)[i] = 0;
@@ -216,86 +191,6 @@ public class Grid3 {
                 result.put(g, gridValues.get(g));
         }
         gridValues = result;
-    }
-
-    public void addTrajectory(ArrayList<Point> trajectory) {
-        for (int i = 1; i < trajectory.size() - 1; i++) {
-            Point p1 = trajectory.get(i-1);
-            Point p2 = trajectory.get(i);
-            double distance = util.getDistancePointToPoint(p1, p2);
-            double ang = 0;
-            if(p2.getY() < p1.getY())
-                ang = util.getAngle(p2, p1);
-            else
-                ang = util.getAngle(p1, p2);
-            int angleIndex = (int) Math.floor((angles * ((ang + Math.PI / (angles * 2)) / (Math.PI)))) % angles;
-            GridPosition g1 = getGridPosition(p1);
-            double[] angleArray = new double[angles];
-            // Add probability for start point;
-            if (gridValues.containsKey(g1)) {
-                angleArray = gridValues.get(g1);
-            }
-            angleArray[angleIndex] += 1.0;
-            gridValues.put(g1, angleArray);
-            // Add probability for start point neighbors
-            ArrayList<GridPosition> neighbors = getNeighbors(g1);
-            for (GridPosition n : neighbors) {
-                if (!gridValues.containsKey(n)) {
-                    angleArray = new double[angles];
-                } else {
-                    angleArray = gridValues.get(n);
-                }
-                angleArray[angleIndex] += 0.5;
-                gridValues.put(n, angleArray);
-            }
-            // Add probability for gridpositions between points
-            GridPosition g2 = getGridPosition(p2);
-            if (g1.getX() > g2.getX()) {
-                g1 = g2;
-                g2 = getGridPosition(p1);
-            }
-            int step = 0;
-            double x = g1.getX();
-            int xSteps = (int) Math.abs(g2.getX() - g1.getX());
-            double yChange = (g2.getY() - g1.getY()) / xSteps;
-            while (x != g2.getX()) {
-                if(step != 0){
-                    double yVal = g1.getY() + (step*yChange);
-                    int y = (int) ((yVal-yMin)/yPixelWidth);
-                    GridPosition newG = new GridPosition(x, y);
-                    if (!gridValues.containsKey(newG)) {
-                        angleArray = new double[angles];
-                    } else {
-                        angleArray = gridValues.get(newG);
-                    }
-                    angleArray[angleIndex] += 1/distance;
-                    gridValues.put(newG, angleArray);
-                }
-                step++;
-                x++;
-            }
-            // Add probability for last point in trajectory
-            if(i == trajectory.size()-1){
-                g2 = getGridPosition(p2);
-                if (!gridValues.containsKey(g2)) {
-                    angleArray = new double[angles];
-                } else {
-                    angleArray = gridValues.get(g2);
-                }
-                angleArray[angleIndex] += 1.0;
-                gridValues.put(g2, angleArray);
-                neighbors = getNeighbors(g2);
-                for (GridPosition n : neighbors) {
-                    if (!gridValues.containsKey(n)) {
-                        angleArray = new double[angles];
-                    } else {
-                        angleArray = gridValues.get(n);
-                    }
-                    angleArray[angleIndex] += 0.5;
-                    gridValues.put(n, angleArray);
-                }
-            }
-        }
     }
 
     public ArrayList<GridPosition> getNeighbors(GridPosition g){
--- Map Construction Java/src/MapConstruction2.java
+++ Map Construction Java/src/MapConstruction2.java
@@ -18,13 +18,12 @@ public class MapConstruction2 {
         this.data = data;
         this.minLatLon = minLatLon;
         this.maxLatLon = maxLatLon;
-        //filterPoints();
+        filterPoints();
         sanityCheck();
         grid = new Grid3(5, 5, 8, new UTMPoint(minLatLon), new UTMPoint(maxLatLon), 2);
         for(Integer key : data.keySet()){
             ArrayList<Point> list = data.get(key);
             for(int i=1; i<list.size();i++){
-                //if(list.get(i-1).getX()==497588.0 && list.get(i-1).getY()==6224757.0)
                 grid.addSegment(list.get(i-1), list.get(i));
             }
         }
